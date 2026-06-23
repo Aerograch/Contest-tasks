2483cf5260cb0839495a26a5c3ef77afef2b5cda
aerograch <aerograchalr@gmail.com>
2026-06-23 23:40:14 +0300

    Добавьте файлы проекта.

---
+++ Task 1/Class1.cs
@@ -0,0 +1,107 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Task_1
+{
+    public static class Encoding
+    {
+        /// <summary>
+        /// Encodes an arbitrary string consisting of lowercase latin characters.
+        /// Format of encoding "aabbbccd" => "a2b3c2d"
+        /// </summary>
+        /// <exception cref="FormatException">
+        /// Thrown when string contains anything else than lowercase latin characters
+        /// </exception>
+        public static string Encode(string input)
+        {
+            // Input validation
+            string requieredPattern = "^[a-z]+$";
+            if (!Regex.IsMatch(input, requieredPattern))
+                throw new FormatException();
+
+            // Encoding
+            char previousChar;
+            int counter = 1;
+            string output = "";
+            for (int i = 1; i < input.Length; i++)
+            {
+                previousChar = input[i - 1];
+                if (input[i] != previousChar)
+                {
+                    output += previousChar;
+                    if (counter > 1)
+                    {
+                        output += counter;
+                    }
+                    counter = 0;
+                }
+                counter++;
+            }
+
+            output += input[input.Length - 1];
+            if (counter > 1)
+            {
+                output += counter;
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Decodes a string consisting of numbers and lowercase latin characters.
+        /// Format of decoding "a2b3c2d" => "aabbbccd"
+        /// </summary>
+        /// <exception cref="FormatException">
+        /// Thrown when string contains anything else than numbers and lowercase latin characters
+        /// </exception>
+        public static string Decode(string input)
+        {
+            // Input validation
+            string requieredPattern = "^[a-z][a-z0-9]*$";
+            if (!Regex.IsMatch(input, requieredPattern))
+                throw new FormatException();
+
+            // Decoding
+            string output = "";
+            int numberOrder = 0;
+            int counter;
+            int charAmount;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                // Check if current char is not number
+                if (input[i] >= '0' && input[i] <= '9') 
+                    continue;
+
+                // Get order of magnitude of amount of current char
+                numberOrder = 0;
+                counter = i + 1;
+                while (counter < input.Length && input[counter] >= '0' && input[counter] <= '9')
+                {
+                    numberOrder++;
+                    counter++;
+                }
+                
+                if (numberOrder == 0)
+                {
+                    output += input[i];
+                }
+                else
+                {
+                    // Get char aount
+                    charAmount = 0;
+                    counter = 0;
+                    for (int j = i + numberOrder; j > i; j--)
+                    {
+                        charAmount += (input[j] - '0') * ((int)Math.Pow(10, counter));
+                        counter++;
+                    }
+
+                    output += new StringBuilder().Append(input[i], charAmount).ToString();
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Tests/Test1.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+using Task_1;
+
+namespace Tests
+{
+    [TestClass]
+    public class EncodingTests
+    {
+        [TestMethod]
+        public void Encode_Simple()
+        {
+            string testInput = "abbcccdddd";
+            string expectedOutput = "ab2c3d4";
+
+            string actualOutput = Task_1.Encoding.Encode(testInput);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void Encode_EndingIsSingle()
+        {
+            string testInput = "kkkljjjjeac";
+            string expectedOutput = "k3lj4eac";
+
+            string actualOutput = Task_1.Encoding.Encode(testInput);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void Encode_OnlySingleCharacters()
+        {
+            string testInput = "heloworld";
+            string expectedOutput = "heloworld";
+
+            string actualOutput = Task_1.Encoding.Encode(testInput);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void Encode_MultiDigitCompression()
+        {
+            string testInput = "aaaaaaaaaaaaaaaaaaccccccdgggggggggggggggggggggg";
+            string expectedOutput = "a18c6dg22";
+
+            string actualOutput = Task_1.Encoding.Encode(testInput);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void Encode_InvalidFormat()
+        {
+            string testInput = ";23";
+            Assert.Throws<FormatException>(() => Task_1.Encoding.Encode(testInput));
+        }
+
+        [TestMethod]
+        public void Decode_Simple()
+        {
+            string testInput = "b2c3d4";
+            string expectedOutput = "bbcccdddd";
+
+            string actualOutput = Task_1.Encoding.Decode(testInput);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void Decode_EndingIsSingle()
+        {
+            string testInput = "k3lj4eac";
+            string expectedOutput = "kkkljjjjeac";
+
+            string actualOutput = Task_1.Encoding.Decode(testInput);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void Decode_StartIsSimple()
+        {
+            string testInput = "ab2c3d4";
+            string expectedOutput = "abbcccdddd";
+
+            string actualOutput = Task_1.Encoding.Decode(testInput);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void Decode_OnlySingleCharacters()
+        {
+            string testInput = "heloworld";
+            string expectedOutput = "heloworld";
+
+            string actualOutput = Task_1.Encoding.Decode(testInput);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void Decode_MultiDigitCompression()
+        {
+            string testInput = "a18c6dg22";
+            string expectedOutput = "aaaaaaaaaaaaaaaaaaccccccdgggggggggggggggggggggg";
+
+            string actualOutput = Task_1.Encoding.Decode(testInput);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void Decode_InvalidFormat()
+        {
+            string testInput = "1a1a4";
+            Assert.Throws<FormatException>(() => Task_1.Encoding.Decode(testInput));
+        }
+    }
+}
